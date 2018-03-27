a56980184b79fd8d755c10ddc07e66f740a02280
Tuhlu <32290251+t6hatu01@users.noreply.github.com>
2018-03-27 13:15:59 +0300

    Add files via upload

--- GameActivity.java
+++ GameActivity.java
@@ -1,10 +1,13 @@
 package pocket.game;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by Tuhlu on 16.3.2018.
@@ -23,6 +26,28 @@ public class GameActivity extends AppCompatActivity {
                 Intent intent = new Intent();
                 setResult(RESULT_OK, intent);
                 finish();
+            }
+        });
+
+        Button Heavy = (Button)findViewById(R.id.Heavy);
+        Heavy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ImageView man = (ImageView)findViewById(R.id.player);
+                man.setImageResource(R.drawable.lpunch);
+                AnimationDrawable hpunch = (AnimationDrawable) man.getDrawable();
+                hpunch.start();
+            }
+        });
+
+        Button button = findViewById(R.id.Right);
+        final ImageView image = findViewById(R.id.player);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ViewGroup.MarginLayoutParams)image.getLayoutParams()).topMargin += 1;
+                image.requestLayout();
             }
         });
     }
--- ShopActivity.java
+++ ShopActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * Created by Tuhlu on 19.3.2018.
  */
 
 public class ShopActivity extends AppCompatActivity {
+
+    int coins = 50;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,32 @@ public class ShopActivity extends AppCompatActivity {
                 Intent intent = new Intent();
                 setResult(RESULT_OK, intent);
                 finish();
+            }
+        });
+
+        ImageButton item = findViewById(R.id.buyButton1);
+        item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (coins > 0)
+                {
+                    coins = coins - 5;
+                    TextView coin = (TextView) findViewById(R.id.textCoins);
+                    coin.setText("" + coins);
+                }
+            }
+        });
+
+        ImageButton item2 = findViewById(R.id.buyButton2);
+        item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (coins >= 30)
+                {
+                    coins = coins - 30;
+                    TextView coin = (TextView) findViewById(R.id.textCoins);
+                    coin.setText("" + coins);
+                }
             }
         });
     }
