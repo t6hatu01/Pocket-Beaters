cc2b1f68bb94bd215bbe00223401de016a4e09b6
DlGLET <37536660+DlGLET@users.noreply.github.com>
2018-04-17 12:33:52 +0300

    Add files via upload
    jee boiii

--- MainActivity.java
+++ MainActivity.java
@@ -1,74 +1,90 @@
-package pocket.game;
+package ga.me;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton playb;
-    private ImageButton shopb;
-    private ImageButton settb;
-    private ImageButton credb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-
-        playb = findViewById(R.id.playButton);
-        playb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                play();
-            }
-        });
-
-        shopb = findViewById(R.id.shopButton);
-        shopb.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+        Button Light = (Button)findViewById(R.id.Light);
+        Light.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                shop();
+
+                ImageView man = (ImageView)findViewById(R.id.player);
+                man.setImageResource(R.drawable.lpunch);
+                AnimationDrawable lpunch = (AnimationDrawable) man.getDrawable();
+                lpunch.start();
             }
         });
 
-        settb = findViewById(R.id.setButton);
-        settb.setOnClickListener(new View.OnClickListener() {
+        Button Heavy = (Button)findViewById(R.id.Heavy);
+        Heavy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                settings();
+
+                ImageView man = (ImageView)findViewById(R.id.player);
+                man.setImageResource(R.drawable.hpunch);
+                AnimationDrawable hpunch = (AnimationDrawable) man.getDrawable();
+                hpunch.start();
             }
         });
 
-        credb = findViewById(R.id.credButton);
-        credb.setOnClickListener(new View.OnClickListener() {
+        final Button Forward = (Button)findViewById(R.id.Forward);
+        final ImageView player = findViewById(R.id.player);
+        Forward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                credits();
+
+                ImageView man = (ImageView)findViewById(R.id.player);
+                man.setImageResource(R.drawable.moveright);
+                AnimationDrawable moveright = (AnimationDrawable) man.getDrawable();
+                moveright.start();
+                RelativeLayout.LayoutParams mParams = (RelativeLayout.LayoutParams)
+                        player.getLayoutParams();
+                mParams.leftMargin += 20;
+                player.setLayoutParams(mParams);
+
             }
         });
-    }
-    public void play(){
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-    }
 
-    public void shop(){
-        Intent intent2 = new Intent(this, ShopActivity.class);
-        startActivity(intent2);
-    }
+        final Button Backward = (Button)findViewById(R.id.Backward);
+        Backward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    public void settings(){
-        Intent intent3 = new Intent(this, SettingsActivity.class);
-        startActivity(intent3);
-    }
+                ImageView man = (ImageView)findViewById(R.id.player);
+                man.setImageResource(R.drawable.backwards);
+                AnimationDrawable backwards = (AnimationDrawable) man.getDrawable();
+                backwards.start();
+                RelativeLayout.LayoutParams mParams = (RelativeLayout.LayoutParams)
+                        player.getLayoutParams();
+                mParams.leftMargin -= 20;
+                player.setLayoutParams(mParams);
+            }
+        });
 
-    public void credits(){
-        Intent intent4 = new Intent(this, CreditActivity.class);
-        startActivity(intent4);
-    }
 
-}
+
+        Button button = findViewById(R.id.blank);
+        final ImageView image = findViewById(R.id.player);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ViewGroup.MarginLayoutParams)image.getLayoutParams()).topMargin += 1;
+                image.requestLayout();
+            }
+        });
+
+
+    }}
